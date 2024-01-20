5fb9a6928222228b601cc4702255fac94613fe75
I wanna be real nerd <71207364+IwannabeRealnerD@users.noreply.github.com>
2024-01-20 22:58:41 +0900

    [optimize] Optimized time complexity of outputCreator Function

--- src/routes/constant.ts
+++ src/routes/constant.ts
@@ -1,3 +1,5 @@
+import { claerStorageArr } from "./util";
+
 export const COMMAND_OBJ = {
 	HELP: "help",
 	ABOUT: "about",
@@ -5,4 +7,24 @@ export const COMMAND_OBJ = {
 	BLOG: "blog",
 	PWD: "pwd",
 	CLEAR: "clear"
+};
+
+export const COMMAND_ACTIONS = {
+	[COMMAND_OBJ.ABOUT]: () =>
+		"khanne-sh is a terminal that has information about khanne(me). \n You can use 'help' command to see available commands.",
+	[COMMAND_OBJ.HELP]: () =>
+		"Here are commands that you can use: \n 1. about : to get information about this project. \n 2. github : open new tab for khanne's github \n 3. help : to see available commands \n 4. pwd : to see current url \n 5. clear : to clear history",
+	[COMMAND_OBJ.GITHUB]: () => {
+		window.open("https://github.com/IwannabeRealnerD");
+		return "Redirecting to khanne's github repositories...";
+	},
+	[COMMAND_OBJ.BLOG]: () => {
+		window.open("https://iwannaberealnerd.oopy.io/develog");
+		return "Opening new tabs for khanne's blog...";
+	},
+	[COMMAND_OBJ.PWD]: () => window.location.href,
+	[COMMAND_OBJ.CLEAR]: () => {
+		claerStorageArr();
+		return "khanne-sh history is cleared.";
+	}
 };
--- src/routes/util.ts
+++ src/routes/util.ts
@@ -2,7 +2,7 @@ import { TERMINAL_HISTORY_KEY } from "$/constants";
 import type { CommandType } from "$/type";
 import { getLocalStorageItem, setLocalStorageItem } from "$/lib/util";
 
-import { COMMAND_OBJ } from "./constant";
+import { COMMAND_ACTIONS, COMMAND_OBJ } from "./constant";
 
 export const isValidCommand = (userInputCommand?: string) => {
 	for (const value of Object.values(COMMAND_OBJ)) {
@@ -26,29 +26,11 @@ export const checkDuplicatedCommand = (
 		.slice(resultArr.length - 3)
 		.every((value) => value === command);
 };
+
 export const outputCreator = (inputCommand: string) => {
-	if (inputCommand === COMMAND_OBJ.ABOUT) {
-		return "khanne-sh is a terminal that has information about khanne(me). \n You can use 'help' command to see available commands.";
-	}
-	if (inputCommand === COMMAND_OBJ.HELP) {
-		return "Here are commands that you can use: \n 1. about : to get information about this project. \n 2. github : open new tab for khanne's github \n 3. help : to see available commands \n 4. pwd : to see current url \n 5. clear : to clear history";
-	}
-	if (inputCommand === COMMAND_OBJ.GITHUB) {
-		window.open("https://github.com/IwannabeRealnerD");
-		return "Redirecting to khanne's github repositories...";
-	}
-	if (inputCommand === COMMAND_OBJ.BLOG) {
-		window.open("https://iwannaberealnerd.oopy.io/develog");
-		return "Opening new tabs for khanne's blog...";
-	}
-	if (inputCommand === COMMAND_OBJ.PWD) {
-		return window.location.href;
-	}
-	if (inputCommand === COMMAND_OBJ.CLEAR) {
-		claerStorageArr();
-		return "khanne-sh history is cleared.";
-	}
-	return `khanne-sh: command not found: ${inputCommand}`;
+	const action = COMMAND_ACTIONS[inputCommand];
+	console.log(action);
+	return action ? action() : `khanne-sh: command not found: ${inputCommand}`;
 };
 
 export const putLocalStorageArr = (commandObj: CommandType) => {
