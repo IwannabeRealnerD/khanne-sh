9dffe1ff62b6bf7dd11fe1de489f7effdeea8224
I wanna be real nerd <71207364+IwannabeRealnerD@users.noreply.github.com>
2024-04-21 14:41:26 +0900

    [env] typo fixed

--- src/routes/components/index.ts
+++ src/routes/components/index.ts
@@ -1,2 +1,2 @@
 export { default as HistoryLine } from "./HistoryLine.svelte";
-export { default as WelcomeMessage } from "./welcomeMessage.svelte";
+export { default as WelcomeMessage } from "./WelcomeMessage.svelte";
