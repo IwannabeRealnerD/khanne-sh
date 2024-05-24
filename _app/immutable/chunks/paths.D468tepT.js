2a86ee4ba722941768245dc654566fe6c9af48ed
I wanna be real nerd <71207364+IwannabeRealnerD@users.noreply.github.com>
2024-05-24 20:52:05 +0900

    Updates

---
+++ _app/immutable/chunks/paths.D468tepT.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1qbbpzx)==null?void 0:s.base)??"/khanne-sh";var a;const b=((a=globalThis.__sveltekit_1qbbpzx)==null?void 0:a.assets)??e;export{b as a,e as b};
---
+++ _app/immutable/entry/start.C7QI0vrv.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CfN3Rnyo.js";export{t as start};
