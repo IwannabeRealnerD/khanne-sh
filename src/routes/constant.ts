1b266a9380fea0b1ab69636c914c65981e9dedf2
I wanna be real nerd <71207364+IwannabeRealnerD@users.noreply.github.com>
2024-05-26 14:25:40 +0900

    Changed 'help' command to make less mistakes - to not forget to add new commands to help command

--- src/routes/constant.ts
+++ src/routes/constant.ts
@@ -13,6 +13,17 @@ export type TCommandValues = (typeof COMMANDS)[TCommandKeys];
 type COMMAND_ACTIONS_DEF = {
 	[P in TCommandValues]: () => string;
 };
+type HELP_OUTPUT_DEF = { [P in TCommandValues]: string };
+
+export const HELP_COMMAND: HELP_OUTPUT_DEF = {
+	about: "to get information about this project",
+	blog: "open new window for khanne's blog",
+	clear: "to clear history",
+	contact: "to get khanne's contact",
+	github: "open new window for khanne's github",
+	help: "to see available commands",
+	pwd: "to see current url"
+};
 
 export const COMMAND_ACTIONS: COMMAND_ACTIONS_DEF = {
 	[COMMANDS.ABOUT]: () =>
@@ -27,8 +38,16 @@ export const COMMAND_ACTIONS: COMMAND_ACTIONS_DEF = {
 	[COMMANDS.CONTACT]: () => {
 		return "- Email: iwannaberealnerd@gmail.com\n- Phone Number: +821051200592\n- LinkedIn: www.linkedin.com/in/honggwanjeong";
 	},
-	[COMMANDS.HELP]: () =>
-		"Here are commands that you can use: \n 1. about : to get information about this project. \n 2. github : open new tab for khanne's github \n 3. help : to see available commands \n 4. pwd : to see current url \n 5. clear : to clear history",
+	[COMMANDS.HELP]: () => {
+		let commandOutput = "Here are commands that you can use: \n";
+		Object.entries(HELP_COMMAND).forEach(([command, commandResult], index) => {
+			commandOutput = commandOutput.concat(
+				` ${index + 1}. ${command} : ${commandResult}\n`
+			);
+			console.log("here");
+		});
+		return commandOutput;
+	},
 	[COMMANDS.GITHUB]: () => {
 		window.open("https://github.com/IwannabeRealnerD");
 		return "Redirecting to khanne's github repositories...";
