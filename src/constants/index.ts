35ecd69a2d9f88ceee7f3cbef1c27028b5e9a096
I wanna be real nerd <71207364+IwannabeRealnerD@users.noreply.github.com>
2024-05-27 09:03:06 +0900

    solution doesn't work

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,3 @@
 export * from "./localStorageKey";
 export * from "./link";
+export * from "./testConstant";
--- src/quokkaTestWorking.ts
+++ src/quokkaTestWorking.ts
@@ -2,3 +2,4 @@ import { TEST_CONSTANT } from "./constants/testConstant";
 const a = 3 + 4;
 
 a;
+TEST_CONSTANT;
