7a27d1e7fff78b150c74df87c9d97435a1f04769
I wanna be real nerd <71207364+IwannabeRealnerD@users.noreply.github.com>
2024-06-02 16:17:34 +0900

    directory structure changed (#8)

--- src/test/unitTest/findAvailableCommand.spec.ts
+++ src/test/unitTest/findAvailableCommand.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { findAvailableCommand } from "$/routes/util";
+import { findAvailableCommand } from "$routes/util";
 
 describe("findAvailableCommand()", () => {
 	it('should return "help" when input is "he', () => {
