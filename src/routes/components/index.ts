82d090945c159c443f0f52dbdab1a3bd0f41700c
I wanna be real nerd <71207364+IwannabeRealnerD@users.noreply.github.com>
2024-12-23 21:30:18 +0900

    build: fixing file name mismatch because of git settings

--- src/routes/components/index.ts
+++ src/routes/components/index.ts
@@ -1,3 +1,3 @@
 export { default as HistoryLine } from "./HistoryLine.svelte";
-export { default as WelcomeMessage } from "./WelcomeMessage.svelte";
+export { default as WelcomeMessage } from "./UserInputForm/WelcomeMessage.svelte";
 export { default as UserInputForm } from "./UserInputForm/index.svelte";
